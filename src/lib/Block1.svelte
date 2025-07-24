959cd3b1849a17e3f27623ee7990205187f91960
Vadim <tolkott35@gmail.com>
2025-07-24 17:46:19 +0500

    refactoring blocks 1 & 2

---
+++ src/lib/Block1.svelte
@@ -0,0 +1,51 @@
+<script lang='ts'>
+
+	type IButton = {
+		label: string;
+		href?: string;
+	}
+
+	let { 
+		title,
+		description,
+		button,
+		isReverse = false
+	} : {
+		title?  : string;
+		description? : string;
+		button? : IButton;
+		isReverse? : boolean;
+	} = $props();
+</script>
+
+{#snippet buttonSnippet(button: IButton)}
+	<a href={button?.href} class='lg:w-fit w-full px-6 py-4 rounded-[8px] font-medium text-base text-white bg-[#8A24F3]'>
+		{button.label}
+	</a>
+{/snippet}
+
+<div class:lg:flex-row-reverse={isReverse} class='py-6 w-full flex lg:flex-row flex-col lg:gap-16 gap-6 items-center'>
+	<img src='image.png' alt='' class='lg:w-1/2 w-full rounded-3xl lg:aspect-auto aspect-4/3 object-cover'>
+	<div class='lg:w-1/2 w-full flex flex-col gap-[16px] -order-1'>
+		<div class='flex flex-col gap-3'>
+			<h1 class='font-[500] lg:text-4xl text-2xl'>
+				{@html title}
+			</h1>
+			{#if description}
+				<div class='lg:text- text-[16px]'>
+					{@html description}
+				</div>
+			{/if}
+		</div>
+		<div class='{isReverse ? 'lg:flex hidden' : 'flex'}'>
+			{#if button}
+				{@render buttonSnippet(button)}
+			{/if}
+		</div>
+	</div>
+	<div class='{isReverse ? 'lg:hidden flex w-full' : 'hidden'}'>
+		{#if button}
+			{@render buttonSnippet(button)}
+		{/if}
+	</div>
+</div>
---
+++ src/lib/Block2.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+
+    type IBlock = {
+        title: string;
+        description?: string;
+        imageUrl?: string;
+    };
+
+    let { 
+        title,
+        blocks,
+        ...other
+    } : {
+        title?: string;
+        blocks: IBlock[];
+        class?: string;
+    } = $props();
+
+</script>
+
+<div class='flex flex-col lg:gap-6 gap-4 {other?.class}'>
+    <h3 class='lg:text-4xl text-2xl font-medium text-[#171E27]'>{@html title}</h3>
+    <div class='grid grid-cols-1 lg:grid-cols-3 lg:gap-5 gap-4'>
+        {#each blocks as block, index}
+            <div class='relative flex flex-col gap-2.5 justify-between rounded-3xl bg-[#F5F7F9]'>
+                <div class='pl-5 pt-5 flex flex-col gap-3'>
+                    <h3 class='text-2xl font-medium'>{@html block.title}</h3>
+                    <div class='text-base'>{@html block.description}</div>
+                </div>
+                <div class='flex justify-end items-end rounded-b-3xl overflow-hidden'>
+                    <h1 class='absolute left-5 bottom-1 font-medium text-4xl text-[#EDF1F4]'>{index + 1}</h1>
+                    <img src={block.imageUrl} alt=''>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
